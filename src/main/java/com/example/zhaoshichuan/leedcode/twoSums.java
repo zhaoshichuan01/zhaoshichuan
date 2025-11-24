303e5092510de78cf7959dbae16a6c528880592a
zhaoshichuan <zsc2569497392@163.com>
2025-11-24 21:53:09 +0800

    func home

---
+++ src/main/java/com/example/zhaoshichuan/leedcode/twoSums.java
@@ -0,0 +1,15 @@
+package com.example.zhaoshichuan.leedcode;
+
+public class twoSums {
+    public int[] twoSum(int[] nums, int target) {
+        return null;
+
+    }
+
+
+    public static void main(String[] args) {
+        twoSums twoSums = new twoSums();
+        System.out.println(twoSums.twoSum(null, 1));
+    }
+
+}
