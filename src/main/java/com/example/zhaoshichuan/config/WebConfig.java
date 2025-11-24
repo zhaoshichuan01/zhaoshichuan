7e141b72cab0094033296f702dacc4c0895f5edc
zhaoshichuan <zsc2569497392@163.com>
2025-11-25 00:26:33 +0800

    func home

--- src/main/java/com/example/zhaoshichuan/config/WebConfig.java
+++ src/main/java/com/example/zhaoshichuan/config/WebConfig.java
@@ -4,6 +4,7 @@ package com.example.zhaoshichuan.config;
 import com.example.zhaoshichuan.interceptor.LoginCheckInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,8 +16,17 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Autowired
     LoginCheckInterceptor loginCheckInterceptor;
+    
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         //registry.addInterceptor(loginCheckInterceptor).addPathPatterns("/**").excludePathPatterns("/login");
+    }
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
     }
 }
