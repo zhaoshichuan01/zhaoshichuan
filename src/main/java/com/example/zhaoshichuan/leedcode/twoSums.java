de265ffe121ea142f4e2e1be9f59523677205948
zhaoshichuan <zsc2569497392@163.com>
2025-12-14 14:43:11 +0800

    func home

--- src/main/java/com/example/zhaoshichuan/leedcode/twoSums.java
+++ src/main/java/com/example/zhaoshichuan/leedcode/twoSums.java
@@ -1,15 +1,39 @@
 package com.example.zhaoshichuan.leedcode;
 
-public class twoSums {
-    public int[] twoSum(int[] nums, int target) {
-        return null;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
+public class twoSums {
+    // https://leetcode-cn.com/problems/two-sum/
+    public int[] twoSum1(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++){
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return new int[] {};
+
+    }
+
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i<nums.length;i++){
+            int key = target - nums[i];
+            if (map.containsKey(key)){
+                return new int[]{map.get(key), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
     }
 
 
     public static void main(String[] args) {
         twoSums twoSums = new twoSums();
-        System.out.println(twoSums.twoSum(null, 1));
+        System.out.println(Arrays.toString(twoSums.twoSum2(new int[]{2,7,11,15}, 9)));
     }
 
 }
